d516b07473ea3f9f9a42f46208fd33407b25ef99
huangkaiwen <1054547034@qq.com>
2022-10-24 13:54:54 +0800

    s

--- src/views/three/flutterPicture.vue
+++ src/views/three/flutterPicture.vue
@@ -15,14 +15,15 @@ console.log("Water", Water)
 export default defineComponent({
     name: "flutterPicture",
     setup() {
-      let container: unknown = null;
+      let container: any = null;
       let stats: unknown = null;
-      let scene: unknown = null;
-      let renderer: unknown = null;
+      let scene: any = null;
+      let renderer: any = null;
       let controls: unknown = null;
-      let water: unknown = null;
+      let water: any = null;
       let sun: unknown = null;
       let mesh: unknown = null;
+      let camera: any = null;
       return {
         container,
         stats,
@@ -32,16 +33,41 @@ export default defineComponent({
         water,
         sun,
         mesh,
+        camera,
       };
     },
 methods: {
     init() {
-        this.container = document.getElementById('container') as HTMLElement;
-        this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setPixelRatio( window.devicePixelRatio );
-        this.renderer.setSize( window.innerWidth, window.innerHeight );
-        this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
-        this.container.appendChild( this.renderer.domElement );
+      this.container = document.getElementById('container') as HTMLElement;
+      this.renderer = new THREE.WebGLRenderer()
+      this.renderer.setPixelRatio( window.devicePixelRatio );
+      this.renderer.setSize( window.innerWidth, window.innerHeight );
+      this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+      this.container.appendChild( this.renderer.domElement );
+      this.scene = new THREE.Scene();
+      this.camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 1, 20000 );
+			this.camera.position.set( 30, 30, 100 );
+      this.sun = new THREE.Vector3();
+      const waterGeometry = new THREE.PlaneGeometry( 10000, 10000 );
+      this.water = new Water(
+        waterGeometry,
+        {
+          textureWidth: 512,
+          textureHeight: 512,
+          waterNormals: new THREE.TextureLoader().load( 'textures/waternormals.jpg', function ( texture ) {
+
+            texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+          } ),
+          sunDirection: new THREE.Vector3(),
+          sunColor: 0xffffff,
+          waterColor: 0x001e0f,
+          distortionScale: 3.7,
+          fog: this.scene.fog !== undefined
+        }
+      );
+      this.water.rotation.x = - Math.PI / 2;
+			this.scene.add( this.water );
     },
     animate() {},
 },
